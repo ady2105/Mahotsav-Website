36994ec9ed097395d12c4434f2958f5f05b031d7
MOHD Zeeshan Quraishi <zq8281@gmail.com>
2026-02-12 20:07:40 +0530

    new event added

--- server-Mahotsav/models/participants.js
+++ server-Mahotsav/models/participants.js
@@ -59,6 +59,7 @@ const Kirigami = mongoose.model("Kirigami", individualSchema);
 const Calligraphy = mongoose.model("Calligraphy", individualSchema);
 const Scandal = mongoose.model("Scandal", individualSchema);
 const Comicstan = mongoose.model("Comicstan", individualSchema);
+const OpenMic = mongoose.model("OpenMic", individualSchema);
 
 const PaperInVogue = mongoose.model("Paper-In-Vogue", teamSchema);
 const SpotSketching = mongoose.model("Spot-Sketching", teamSchema);
@@ -71,6 +72,7 @@ module.exports = {
   teamSchema,
   individualSchema,
   Kirigami,
+  OpenMic,
   Calligraphy,
   Scandal,
   Comicstan,
